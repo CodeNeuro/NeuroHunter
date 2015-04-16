b98f57e470d434a05570c1263038e528b3933589
freeman <the.freeman.lab@gmail.com>
2015-04-15 22:32:08 -0400

    Fix submission

--- submissions/freeman-lab/run.py
+++
@@ -1,8 +0,0 @@
-def run(data):
-    """
-    Run a source extraction algorithm on an Images object
-    """
-    from thunder import SourceExtraction
-    method = SourceExtraction('localmax')
-    result = method.fit(data)
-    return result
