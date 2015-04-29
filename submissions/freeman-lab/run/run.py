58c8c29f684bb58cb1690c8bf5d61f45128854e5
freeman <the.freeman.lab@gmail.com>
2015-04-29 11:35:42 -0400

    Change algorithm

--- submissions/freeman-lab/run/run.py
+++ submissions/freeman-lab/run/run.py
@@ -3,6 +3,6 @@ def run(data):
     Run a Thunder source extraction algorithm
     """
     from thunder import SourceExtraction
-    method = SourceExtraction('localmax')
+    method = SourceExtraction('nmf')
     result = method.fit(data)
     return result
